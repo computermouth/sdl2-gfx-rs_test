89596269938ffdc4272887a3237237bd9fbd1921
computermouth <computermouth@crunchbangplusplus.org>
2015-10-14 13:49:23 -0500

    Implement face shapes

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,49 @@ const SCREEN_HEIGHT: u32 = 600;
 
 fn main() {
 
+	let faceX = vec![ 178, 524, 616, 621, 597, 549, 531, 508, 238, 102, 32, 23, 50, 47, 93]; 
+	let faceY = vec![ 85, 193, 465, 570, 632, 667, 645, 697, 878, 842, 689, 534, 413, 344, 199];
+					   
+	let leftNostrilX = vec![ 111, 133, 125, 110]; 
+	let leftNostrilY = vec![ 521, 532, 543, 533];
+
+	let rightNostrilX = vec![ 161, 186, 201, 195]; 
+	let rightNostrilY = vec![ 529, 525, 533, 548];
+
+	let lipsX = vec![ 112, 132, 174, 210, 268, 185, 117, 83]; 
+	let lipsY = vec![ 609, 615, 605, 637, 663, 703, 697, 655];
+
+	let teethX = vec![ 123, 139, 179, 199, 164]; 
+	let teethY = vec![ 644, 637, 631, 648, 656];
+
+	let leftBrowX = vec![ 43, 93, 150, 141, 49]; 
+	let leftBrowY = vec![ 332, 314, 325, 342, 349];
+
+	let rightBrowX = vec![ 298, 375, 445, 366, 293]; 
+	let rightBrowY = vec![ 305, 311, 354, 348, 338];
+
+	let leftWhiteX = vec![ 85, 113, 145, 141, 85, 72]; 
+	let leftWhiteY = vec![ 374, 373, 385, 397, 408, 394];
+
+	let rightWhiteX = vec![ 320, 361, 394, 370, 321, 297]; 
+	let rightWhiteY = vec![ 372, 378, 403, 406, 407, 393];
+
+	let leftIrisX = vec![ 92, 109, 121, 104, 84, 79]; 
+	let leftIrisY = vec![ 369, 371, 388, 402, 401, 382];
+
+	let rightIrisX = vec![ 318, 342, 362, 359, 342, 319, 311]; 
+	let rightIrisY = vec![ 369, 370, 381, 394, 409, 406, 391];
+
+	let leftPupilX = vec![ 97, 104, 97, 92]; 
+	let leftPupilY = vec![ 374, 387, 396, 387];
+
+	let rightPupilX = vec![ 334, 342, 335, 326]; 
+	let rightPupilY = vec![ 377, 389, 397, 386];
+
+	let hairX = vec![ 92, 73, 109, 212, 321, 438, 519, 597, 622, 618, 572, 564, 528, 532, 495, 484, 422, 323, 198, 169, 128]; 
+	let hairY = vec![ 203, 174, 98, 29, 13, 42, 102, 215, 319, 476, 456, 521, 531, 455, 336, 226, 174, 144, 159, 141, 163];
+
+
     let sdl_context = sdl2::init().unwrap();
     let video_subsys = sdl_context.video().unwrap();
     let window = video_subsys.window("sdl2-gfx-rs_test", SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -47,10 +90,20 @@ fn main() {
             }
         }
         
-        let vx = vec![200, 500, 500, 200];
-        let vy = vec![200, 200, 500, 500];
         let color = pixels::Color::RGBA(255, 0, 0, 255);
-        let _ = renderer.filled_polygon(&vx, &vy, color);
+        let _ = renderer.filled_polygon(&faceX, &faceY, color);
+        let _ = renderer.filled_polygon(&leftNostrilX, &leftNostrilY, color);
+        let _ = renderer.filled_polygon(&rightNostrilX, &rightNostrilY, color);
+        let _ = renderer.filled_polygon(&lipsX, &lipsY, color);
+        let _ = renderer.filled_polygon(&teethX, &teethY, color);
+        let _ = renderer.filled_polygon(&leftBrowX, &rightBrowY, color);
+        let _ = renderer.filled_polygon(&leftWhiteX, &leftWhiteY, color);
+        let _ = renderer.filled_polygon(&rightWhiteX, &rightWhiteY, color);
+        let _ = renderer.filled_polygon(&leftIrisX, &leftIrisY, color);
+        let _ = renderer.filled_polygon(&rightIrisX, &rightIrisY, color);
+        let _ = renderer.filled_polygon(&leftPupilX, &leftPupilY, color);
+        let _ = renderer.filled_polygon(&rightPupilX, &rightPupilY, color);
+        let _ = renderer.filled_polygon(&hairX, &hairY, color);
         
         renderer.present();
     }
