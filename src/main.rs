cd7141ba01fc0847c35ec0e9cede963ec52673ae
computermouth <computermouth@crunchbangplusplus.org>
2015-10-15 19:21:00 -0500

    Pushed all vectors to shape structs

--- src/main.rs
+++ src/main.rs
@@ -23,41 +23,67 @@ fn main() {
 	let left_nostril_x  : Vec<i16> = vec![ 111, 133, 125, 110]; 
 	let left_nostril_y  : Vec<i16> = vec![ 521, 532, 543, 533];
 
+	let left_nostril = facevar::Shape { x: left_nostril_x, y: left_nostril_y};
+	
 	let right_nostril_x : Vec<i16> = vec![ 161, 186, 201, 195]; 
 	let right_nostril_y : Vec<i16> = vec![ 529, 525, 533, 548];
+
+	let right_nostril = facevar::Shape { x: right_nostril_x, y: right_nostril_y};
 
 	let lips_x : Vec<i16> = vec![ 112, 132, 174, 210, 268, 185, 117, 83]; 
 	let lips_y : Vec<i16> = vec![ 609, 615, 605, 637, 663, 703, 697, 655];
 
+	let lips = facevar::Shape { x: lips_x, y: lips_y};
+
 	let teeth_x : Vec<i16> = vec![ 123, 139, 179, 199, 164]; 
 	let teeth_y : Vec<i16> = vec![ 644, 637, 631, 648, 656];
+
+	let teeth = facevar::Shape { x: teeth_x, y: teeth_y};
 
 	let left_brow_x : Vec<i16> = vec![ 43, 93, 150, 141, 49]; 
 	let left_brow_y : Vec<i16> = vec![ 332, 314, 325, 342, 349];
 
+	let left_brow = facevar::Shape { x: left_brow_x, y: left_brow_y};
+
 	let right_brow_x : Vec<i16> = vec![ 298, 375, 445, 366, 293]; 
 	let right_brow_y : Vec<i16> = vec![ 305, 311, 354, 348, 338];
+
+	let right_brow = facevar::Shape { x: right_brow_x, y: right_brow_y};
 
 	let left_white_x : Vec<i16> = vec![ 85, 113, 145, 141, 85, 72]; 
 	let left_white_y : Vec<i16> = vec![ 374, 373, 385, 397, 408, 394];
 
+	let left_white = facevar::Shape { x: left_white_x, y: left_white_y};
+
 	let right_white_x : Vec<i16> = vec![ 320, 361, 394, 370, 321, 297]; 
 	let right_white_y : Vec<i16> = vec![ 372, 378, 403, 406, 407, 393];
+
+	let right_white = facevar::Shape { x: right_white_x, y: right_white_y};
 
 	let left_iris_x : Vec<i16> = vec![ 92, 109, 121, 104, 84, 79]; 
 	let left_iris_y : Vec<i16> = vec![ 369, 371, 388, 402, 401, 382];
 
+	let left_iris = facevar::Shape { x: left_iris_x, y: left_iris_y};
+
 	let right_iris_x : Vec<i16> = vec![ 318, 342, 362, 359, 342, 319, 311]; 
 	let right_iris_y : Vec<i16> = vec![ 369, 370, 381, 394, 409, 406, 391];
+
+	let right_iris = facevar::Shape { x: right_iris_x, y: right_iris_y};
 
 	let left_pupil_x : Vec<i16> = vec![ 97, 104, 97, 92]; 
 	let left_pupil_y : Vec<i16> = vec![ 374, 387, 396, 387];
 
+	let left_pupil = facevar::Shape { x: left_pupil_x, y: left_pupil_y};
+
 	let right_pupil_x : Vec<i16> = vec![ 334, 342, 335, 326]; 
 	let right_pupil_y : Vec<i16> = vec![ 377, 389, 397, 386];
 
+	let right_pupil = facevar::Shape { x: right_pupil_x, y: right_pupil_y};
+
 	let hair_x : Vec<i16> = vec![ 92, 73, 109, 212, 321, 438, 519, 597, 622, 618, 572, 564, 528, 532, 495, 484, 422, 323, 198, 169, 128]; 
 	let hair_y : Vec<i16> = vec![ 203, 174, 98, 29, 13, 42, 102, 215, 319, 476, 456, 521, 531, 455, 336, 226, 174, 144, 159, 141, 163];
+
+	let hair = facevar::Shape { x: hair_x, y: hair_y};
 
 
     let sdl_context = sdl2::init().unwrap();
@@ -98,31 +124,31 @@ fn main() {
         let mut color = pixels::Color::RGBA(249, 188, 188, 255);
         let _ = renderer.filled_polygon(&face.x, &face.y, color);
         color = pixels::Color::RGBA(148, 101, 101, 255);
-        let _ = renderer.filled_polygon(&left_nostril_x, &left_nostril_y, color);
+        let _ = renderer.filled_polygon(&left_nostril.x, &left_nostril.y, color);
         color = pixels::Color::RGBA(148, 101, 101, 255);
-        let _ = renderer.filled_polygon(&right_nostril_x, &right_nostril_y, color);
+        let _ = renderer.filled_polygon(&right_nostril.x, &right_nostril.y, color);
         color = pixels::Color::RGBA(249, 249, 249, 255);
-        let _ = renderer.filled_polygon(&lips_x, &lips_y, color);
+        let _ = renderer.filled_polygon(&lips.x, &lips.y, color);
         color = pixels::Color::RGBA(249, 249, 249, 255);
-        let _ = renderer.filled_polygon(&teeth_x, &teeth_y, color);
+        let _ = renderer.filled_polygon(&teeth.x, &teeth.y, color);
         color = pixels::Color::RGBA(191, 148, 0, 255);
-        let _ = renderer.filled_polygon(&left_brow_x, &left_brow_y, color);
+        let _ = renderer.filled_polygon(&left_brow.x, &left_brow.y, color);
         color = pixels::Color::RGBA(191, 148, 0, 255);
-        let _ = renderer.filled_polygon(&right_brow_x, &right_brow_y, color);
+        let _ = renderer.filled_polygon(&right_brow.x, &right_brow.y, color);
         color = pixels::Color::RGBA(249, 249, 249, 255);
-        let _ = renderer.filled_polygon(&left_white_x, &left_white_y, color);
+        let _ = renderer.filled_polygon(&left_white.x, &left_white.y, color);
         color = pixels::Color::RGBA(249, 249, 249, 255);
-        let _ = renderer.filled_polygon(&right_white_x, &right_white_y, color);
+        let _ = renderer.filled_polygon(&right_white.x, &right_white.y, color);
         color = pixels::Color::RGBA(100, 157, 97, 255);
-        let _ = renderer.filled_polygon(&left_iris_x, &left_iris_y, color);
+        let _ = renderer.filled_polygon(&left_iris.x, &left_iris.y, color);
         color = pixels::Color::RGBA(100, 157, 97, 255);
-        let _ = renderer.filled_polygon(&right_iris_x, &right_iris_y, color);
+        let _ = renderer.filled_polygon(&right_iris.x, &right_iris.y, color);
         color = pixels::Color::RGBA(10, 10, 10, 255);
-        let _ = renderer.filled_polygon(&left_pupil_x, &left_pupil_y, color);
+        let _ = renderer.filled_polygon(&left_pupil.x, &left_pupil.y, color);
         color = pixels::Color::RGBA(10, 10, 10, 255);
-        let _ = renderer.filled_polygon(&right_pupil_x, &right_pupil_y, color);
+        let _ = renderer.filled_polygon(&right_pupil.x, &right_pupil.y, color);
         color = pixels::Color::RGBA(180, 155, 0, 255);
-        let _ = renderer.filled_polygon(&hair_x, &hair_y, color);
+        let _ = renderer.filled_polygon(&hair.x, &hair.y, color);
         
         renderer.present();
     }
